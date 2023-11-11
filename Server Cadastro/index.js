af5d38e6093fb7e256ed5fda03d41dbee1fcf8b9
GreatestSteph <tatazinha_madia@hotmail.com>
2023-11-10 21:25:22 -0300

    Primeiras configurações do server e proteção contra acessos indevidos

--- Server Cadastro/index.js
+++ Server Cadastro/index.js
@@ -1,17 +1,71 @@
-import http from 'http';
-import express from 'express';
+import express from 'express'; //importa express
+import session from 'express-session'; //importa session do express
+import autenticaçaologin, {UsuarioAutenticado} from './segurança/autenticar.js'; 
+//importa as funçoes autenticaçaologin e UsuarioAutenticado do arquivo autenticarjs
 
-const hostname = 'localhost';
-const porta = 1515;
+const localizaçaohostname = 'localhost'; //local do server
+const numeroporta = 1515; //numero da porta
+const aplicacao = express();  
+//cria uma aplicação como instancia do express
 
-const aplicaçao = express()
-aplicaçao.use(express.static('./public'));
-aplicaçao.use(express.static('./protegido'));
-aplicaçao.use(express.static('./login', (requisicao, resposta) =>{
-    resposta.redirect('/login.html')
-}));
-const servidor = http.createServer(aplicaçao);
+//instalado a partir do npm install session
+aplicacao.use(session({ //configura a sessão/login de determinado usuario
+    secret: 'W0WsUp3rçh4V3s3çRet4W0W', //chavesecreta
+    resave: true, //salva a sessão a cada requisição
+    saveUninitialized: true, 
+    // quando for true, a sessão será salva no armazenamento, 
+    // mesmo que ela não tenha sido modificada durante a requisição
 
-servidor.listen(porta, hostname, () => {
-    console.log('O servidor está funcionando em ' + hostname + ', na porta: ' + porta);
+    // quando for false, a sessão só será salva no armazenamento se for modificada 
+
+    // uma sessao modificada é aquela que se altera o login ou senha para o acesso
+    // uma sessao normal é aquela que o login ou senha são os mesmos para o acesso
+    cookie: { //ok
+        maxAge: 1000 * 60 * 15 // a sessão/login irá durar 15 minutos
+    }
+})) 
+
+
+
+aplicacao.use(express.urlencoded({extended: true})); 
+// as informaçoes enviadas de um formulario/etc
+// são analisadas e então codificadas em um objeto javascript (transformadas)
+
+// Exemplo: 
+// nome: João Silva (normal)
+// nome=Jo%C3%A3o+Silva (codificado)
+
+// quando for true, os dados serao analisados e codificados com o ajuda da biblioteca qs 
+// quando for false, os dados serao analisados e codificados com o ajuda da biblioteca query string
+
+
+
+aplicacao.get('/', (requisicao, resposta) => { //se o usuario digitar apenas /
+    resposta.redirect('/login.html'); //ele será redirecionado para a pagina de login
+}); //ok
+
+aplicacao.get('/login', (requisicao, resposta) => { //se o usuario digitar /login
+    resposta.redirect('/login.html'); //o usuario sera redirecionado para a pagina de login
+});
+
+aplicacao.post('/login', autenticaçaologin); 
+//ao acessar a pagina de login
+//o usuario recebe uma requisiçao e produz uma resposta a partir da funçao autenticaçaologin
+
+
+
+
+aplicacao.use(express.static('./public')); 
+//a aplicaçao disponibiliza os arquivos da pasta public, de forma estática
+
+aplicacao.use(UsuarioAutenticado, express.static('./protegido'));
+//a aplicaçao disponibiliza os arquivos da pasta protegida, de forma estática
+//verifica se o usuario foi autenticado antes de levar ele as paginas da pasta protegida
+
+
+
+
+aplicacao.listen(numeroporta, localizaçaohostname, () => {
+    console.log('O servidor está funcionando em ' + localizaçaohostname + ', na porta: ' + numeroporta); 
+    // o server ao ler ou receber uma requisiçao, vai mostrar uma mensagem
 });
---
+++ Server Cadastro/segurança/autenticar.js
@@ -0,0 +1,29 @@
+export default function autenticaçaologin(requisicao, resposta, next) { //ok
+    const email = "a@email"; //armazena email de login
+    const senha = "a"; //armazena senha de login
+
+    const emailpessoa = requisicao.body.email;
+    //captura as informações de email atraves do "name = email"
+    const senhapessoa = requisicao.body.senha;
+    //captura as informações de senha atraves da "name = senha"
+
+    if (emailpessoa === email && senhapessoa === senha) { 
+        // verifica se o email informado é igual a certo email
+        // verifica se a senha informada é igual a certa senha
+        requisicao.session.autenticado = true; //se o login for sucedido
+        resposta.redirect('/cadastro.html'); //redirecione para a pagina de cadastro
+    } else { //ok
+        requisicao.session.autenticado = false; //se o login for mal-sucedido
+        resposta.redirect('/login.html'); //redirecionada para a pagina de login
+    }
+}
+
+
+export function UsuarioAutenticado(requisicao, resposta, next) { //verifica se o usuario esta logado
+    if (requisicao.session.autenticado) { //se o login for sucedido
+        next(); //continue as açoes
+    } else { //ou entao
+        resposta.redirect('/login.html'); //volte a pagina de login
+    }
+}
+//devemos exportar as funçoes, para que elas possam depois serem importadas
